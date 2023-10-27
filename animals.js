d38255ddfa6efdd018ce2f7f9b1d2ba493e92581
MatheusZottis <mathzottis2970@gmail.com>
2023-10-26 22:07:31 -0300

    adicionando arquivos

---
+++ animals.js
@@ -0,0 +1,9 @@
+const animais = ['macaco', 'tubarão', 'pato', 'leão', 'lontra'];
+
+if (!animais.includes('leão')){
+    console.log('Leão não existe no array animals.');
+}
+
+else{
+    console.log('Existe um leão no array animals.');
+}
---
+++ series.js
@@ -0,0 +1,3 @@
+const series = ['Breaking Bad', 'The Boys', 'La Casa de Papel'];
+
+console.log(series.length === 3);
